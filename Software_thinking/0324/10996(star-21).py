5171c4649ba1f8326fa53dfc6ad13dbb45f72e54
nowhun <nowhun@kookmin.ac.kr>
2026-03-31 14:30:54 +0900

    3월 24일 과제

--- Software_thinking/0324/10996(star-21).py
+++ Software_thinking/0324/10996(star-21).py
@@ -1,4 +1,10 @@
 a = int(input())
 
-for i in range (a):
-    print()
+for i in range(a):
+    if (a%2 == 0):
+        print("* "*(a//2))
+        print(" *"*(a//2))
+    else:
+        print("* "*((a//2)+1))
+        print(" *"*(a//2))
+
